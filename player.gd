f52865de23cb2b424752e1cd2dd0414e24fac2df
zangrim <grimzan9@gmail.com>
2024-05-16 21:01:20 +0800

    Added killing of player.

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+signal hit
+
 # How fast the player moves in meters per second.
 @export var speed = 14
 # The downward acceleartion when in the air, in meters per second
@@ -42,14 +44,9 @@ func _physics_process(delta):
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 	
-	# Moving the Character
-	velocity = target_velocity
-	
 	# Jumping.
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		target_velocity.y = jump_impulse
-	
-	move_and_slide()
 	
 	# Iterate through all collisions that occured this frame
 	for index in range(get_slide_collision_count()):
@@ -70,3 +67,16 @@ func _physics_process(delta):
 				target_velocity.y = bounce_impulse
 				# Prevent further duplicate calls.
 				break
+	
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
+
+
+func die():
+	hit.emit()
+	queue_free()
+
+
+func _on_mob_detector_body_entered(body):
+	die()
